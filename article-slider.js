a66769bd7daa4e71f77289527d13ae5b617dccbb
Logan Miller <logan.miller@forumcomm.com>
2016-08-30 18:50:41 -0500

    Fixes jwplayer slider.

--- article-slider.js
+++ article-slider.js
@@ -1,29 +1,49 @@
-jQuery(document).ready(function () {
+jQuery(document).ready(function() {
     var currentIndex = 0,
-            items = jQuery('.article-slider div.individual-slide'),
-            itemAmt = items.length;
+        items = jQuery('.article-slider div.individual-slide'),
+        itemAmt = items.length;
 
     function cycleItems() {
-        var item = jQuery('.article-slider div.individual-slide').eq(currentIndex);
-        items.hide();
-        item.css('display', 'inline-block');
+        for (var index = 0; index < items.length; index++) {
+            var i = jQuery('.article-slider div.individual-slide').eq(index);
+            if (index === currentIndex) {
+                i.css('width', '100%');
+                i.css('display', 'inline-block');
+                continue;
+            }
+            //JWPlayer derps out when hide() is used. set dimesions to 0 instead.
+            var is_video = isVideo(index);
+            if (is_video) {
+                i.css('width', '0');
+                i.css('height', '0');
+            } else {
+                i.hide();
+            }
+        }
+    }
+
+    function isVideo(index) {
+        if ($('#jwplayer-video-' + index).length) {
+            return true;
+        }
+        return false;
     }
 
     function pauseVideos() {
         try {
-            var state = jwplayer('jwplayer-video-' + currentIndex).getState();
-            if (state === 'playing') {
-                jwplayer('jwplayer-video-' + currentIndex).pause();
+            if ($('#jwplayer-video-' + currentIndex).length) {
+                var state = jwplayer('jwplayer-video-' + currentIndex).getState();
+                if (state === 'playing') {
+                    jwplayer('jwplayer-video-' + currentIndex).pause();
+                }
             }
         } catch (e) {
-            console.log(e);
             //browser does not support doing this, so catch error and continue
         }
     }
     cycleItems();
 
-    jQuery('.next').click(function () {
-        console.log('next item');
+    jQuery('.next').click(function() {
         pauseVideos();
         currentIndex += 1;
         if (currentIndex > itemAmt - 1) {
@@ -32,8 +52,7 @@ jQuery(document).ready(function () {
         cycleItems();
     });
 
-    jQuery('.prev').click(function () {
-        console.log('previous item');
+    jQuery('.prev').click(function() {
         pauseVideos();
         currentIndex -= 1;
         if (currentIndex < 0) {
@@ -42,7 +61,7 @@ jQuery(document).ready(function () {
         cycleItems();
     });
 
-    jQuery('.image-info').on('click', function () { // Show caption on info icon click
+    jQuery('.image-info').on('click', function() { // Show caption on info icon click
         jQuery(this).next('span').toggleClass('image-caption-display');
     });
 
